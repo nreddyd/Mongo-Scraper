6d5fd3e419a0d54d9ef847b117c030c5aaf0d5c2
Neeraja <neerajareddy24@gmail.com>
2018-10-23 14:23:27 -0400

    updated server.js

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ app.use(express.static("public"));
 var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true }
+);
 
 app.get("/scrape", function(req, res) {
   console.log("scrape");
