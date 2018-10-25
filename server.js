56a4a9e465ad81346d11ea5fb293e79bbdcba019
Neeraja <neerajareddy24@gmail.com>
2018-10-25 14:35:25 -0400

    updated to add notes and delete notes for news articles

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ var exphbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
+var path = require("path");
+
 var db = require("./models");
+const Note = require("./models/Note.js"); //require the Note model
 
 var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -68,7 +73,7 @@ app.get("/scrape", function(req, res) {
 
 // Route for getting all Articles from the db
 app.get("/", function(req, res) {
-  db.Article.find({})
+  db.Article.find({ saved: false })
     .then(function(article) {
       res.render("index", { article });
     })
@@ -99,6 +104,67 @@ app.post("/save", function(req, res) {
     });
 });
 
+app.post("/unsave", function(req, res) {
+  db.Article.findOneAndUpdate({ _id: req.body._id }, { saved: false })
+    .then(function(article) {
+      res.redirect("back");
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/delete", function(req, res) {
+  console.log("deleteroute");
+  db.Article.deleteMany({})
+    .then(function(article) {
+      res.render("index", { article });
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+app.get("/notes/:id", function(req, res) {
+  //Query to find the matching id to the passed in it
+  db.Article.findOne({ _id: req.params.id })
+    .populate("notes") //Populate all of the notes associated with it
+    .exec(function(error, doc) {
+      //execute the query
+      if (error) console.log(error);
+      // Otherwise, send the doc to the browser as a json object
+      else {
+        res.json(doc);
+      }
+    });
+});
+
+app.post("/notes/:id", function(req, res) {
+  //create a new note with req.body
+  var newNote = new Note(req.body);
+  //save newNote to the db
+  newNote.save(function(err, doc) {
+    // Log any errors
+    if (err) console.log(err);
+    //find and update the note
+    db.Article.findOneAndUpdate(
+      { _id: req.params.id }, // find the _id by req.params.id
+      { $push: { notes: doc._id } }, //push to the notes array
+      { new: true },
+      function(err, newdoc) {
+        if (err) return handleError(err);
+        console.log("notes saved");
+        res.send(newdoc);
+      }
+    );
+  });
+});
+
+app.get("/deleteNote/:id", function(req, res) {
+  Note.remove({ _id: req.params.id }, function(err, newdoc) {
+    if (err) console.log(err);
+    res.redirect("/saved"); //redirect to reload the page
+  });
+});
 app.listen(PORT, function() {
   console.log("listening on : http://localhost:" + PORT);
 });
